5780e8421aaa44f4e058a4a78b1730df95c1ac1a
2anelle <101650655+2anelle@users.noreply.github.com>
2023-03-20 17:49:17 +0800

    fix script

--- script.js
+++ script.js
@@ -15,5 +15,18 @@ function onAddTaskClicked(event){
     todoListContainer.insertAdjacentHTML('afterbegin',taskHTML);
 }
 
+function onTodoListContainerClicked(event){
+    while (!targetElement.classList.contains("task")){
+        targetElement = targetElement.parentElement;
+    }
+    var checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked){
+        targetElement.classList.add("completed");
+    }else{
+        targetElement.classList.remove("completed");
+    }
+}
+
 // S3 link event to handler
 addTaskButton.addEventListener('click', onAddTaskClicked)
+todoListContainer.addEventListener('click', onTodoListContainerClicked);
