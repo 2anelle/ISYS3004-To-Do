6f4167fc3041a3bb1bb8e899b39d031eafffc3af
2anelle <101650655+2anelle@users.noreply.github.com>
2023-03-20 17:32:19 +0800

    using the template to add tasks

--- script.js
+++ script.js
@@ -4,13 +4,15 @@
 var addTaskButton = document.getElementById("add-task")
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
-
+var templateContainer = document.getElementById(list-item-template)
+var template = templateContainer.innerHTML
 
 // S2 write the behaviour
 function onAddTaskClicked(event){
     var taskName = newTaskInput.value;
     newTaskInput.value = "";
-    todoListContainer.insertAdjacentHTML('afterbegin',taskName);
+    var taskHTML = template.replace("<!--Task_name-->"), taskName;
+    todoListContainer.insertAdjacentHTML('afterbegin',taskHTML);
 }
 
 // S3 link event to handler
